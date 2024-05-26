d6a6775fcfcb5c5b09ef6829b3ebc5e470e5d5b4
xiaoxin <onlinedear@163.com>
2024-05-26 11:45:14 +0800

    Update toolbar.vue

--- web/src/components/toolbar.vue
+++ web/src/components/toolbar.vue
@@ -19,8 +19,8 @@ export default {
 <template>
   <div class="fixed bottom-1/3 right-4 z-50 flex flex-col items-center justify-center gap-4">
     <div class="flex w-9 justify-center gap-2 rounded-xl bg-gray-200 p-1 shadow-lg dark:bg-gray-600">
-      <t-button href="https://github.com/yokingma/search_with_ai" target="_blank" shape="circle" theme="default">
-        <template #icon> <RiGithubLine /></template>
+      <t-button href="https://open.ai-api.com.cn" target="_blank" shape="circle" theme="default">
+        <template #icon> <RiSettingsLine /></template>
       </t-button>
     </div>
     <div class="flex w-9 justify-center gap-2 rounded-xl bg-gray-200 p-1 shadow-lg dark:bg-gray-600">
